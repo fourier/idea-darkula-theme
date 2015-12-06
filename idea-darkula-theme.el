26104e612996a35d1a2bcb63c39ebf108e3b1fd4
Alexey Veretennikov <txm.fourier@gmail.com>
2015-12-06 13:04:31 +0100

    Added paths for for theme and comments

--- idea-darkula-theme.el
+++ idea-darkula-theme.el
@@ -1,3 +1,36 @@
+;;; idea-darkula-theme.el --- Color theme based on IntelliJ IDEA Darkula color theme
+
+;; Copyright (C) 2015 Alexey Veretennikov
+
+;; Author: Alexey Veretennikov <alexey dot veretennikov at gmail dot com>
+;; Keywords: themes
+;; URL: http://github.com/fourier/idea-darkula-theme
+;; Version: 0.1
+;; Package-Requires: ((emacs "24.1"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; To use it, put the following in your Emacs configuration file:
+;;
+;;   (load-theme 'idea-darkula t)
+;;
+;; Requirements: Emacs 24.
+
+;;; Code:
+
 (deftheme idea-darkula
   "Theme based on IntelliJ IDEA darkula theme")
 
@@ -116,5 +149,13 @@
  ;; '(match ((((class color) (min-colors 88) (background light)) (:background "yellow1")) (((class color) (min-colors 88) (background dark)) (:background "RoyalBlue3")) (((class color) (min-colors 8) (background light)) (:foreground "black" :background "yellow")) (((class color) (min-colors 8) (background dark)) (:foreground "white" :background "blue")) (((type tty) (class mono)) (:inverse-video t)) (t (:background "gray"))))
  ;; '(next-error ((t (:inherit (region)))))
  ;; '(query-replace ((t (:inherit (isearch))))))
+
+;;;###autoload
+(when (and (boundp 'custom-theme-load-path)
+           load-file-name)
+  ;; add theme folder to `custom-theme-load-path' when installing over MELPA
+  (add-to-list 'custom-theme-load-path
+               (file-name-as-directory (file-name-directory load-file-name))))
+
 
 (provide-theme 'idea-darkula)
